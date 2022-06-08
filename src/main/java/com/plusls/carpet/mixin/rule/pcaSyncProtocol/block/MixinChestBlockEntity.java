050ba41724861475eee01f0e7f624a5ef055e681
Fallen_Breath <yqx791125833@gmail.com>
2022-06-08 18:40:26 +0800

    1.19

--- src/main/java/com/plusls/carpet/mixin/rule/pcaSyncProtocol/block/MixinChestBlockEntity.java
+++ src/main/java/com/plusls/carpet/mixin/rule/pcaSyncProtocol/block/MixinChestBlockEntity.java
@@ -7,14 +7,13 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.block.entity.ChestBlockEntity;
 import net.minecraft.block.entity.LootableContainerBlockEntity;
-import net.minecraft.client.block.ChestAnimationProgress;
 import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 
 // 由于陷阱箱继承自箱子，因此不用 mixin 陷阱箱
 // implements ChestAnimationProgress 会出错 不知道为啥
 @Mixin(ChestBlockEntity.class)
-public abstract class MixinChestBlockEntity extends LootableContainerBlockEntity implements ChestAnimationProgress {
+public abstract class MixinChestBlockEntity extends LootableContainerBlockEntity {
 
     protected MixinChestBlockEntity(BlockEntityType<?> blockEntityType, BlockPos blockPos, BlockState blockState) {
         super(blockEntityType, blockPos, blockState);
